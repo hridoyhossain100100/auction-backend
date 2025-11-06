76ac3955315d218ed9640115648d3c95be794088
hridoyhossain100100 <134464434+hridoyhossain100100@users.noreply.github.com>
2025-11-06 22:55:44 +0600

    Update User.js

--- User.js
+++ User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// ইউজার স্কিমা (Schema) তৈরি করুন
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -11,13 +10,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    // --- নতুন ফিল্ড ---
     role: {
         type: String,
         enum: ['Admin', 'TeamOwner'], // ভূমিকা শুধু এই দুটি হতে পারবে
-        default: 'TeamOwner'         // নতুন কেউ রেজিস্টার করলে সে 'TeamOwner' হবে
+        default: 'TeamOwner'       // নতুন কেউ রেজিস্টার করলে সে 'TeamOwner' হবে
     },
-    // ---
     team: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Team'
